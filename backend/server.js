69963b92a35a0a218ce5499df1166b2116146964
Chanduadep <adepchandu22@gmail.com>
2024-12-05 13:47:15 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app=express()
 //to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"https://notes-app-backend-sgnn.onrender.com",credentials:true }))
+app.use(cors({origin:"https://notes-app-frontend-g2h0.onrender.com",credentials:true }))
 
 
 app.listen(3000,()=>{
